956956e60ffddc77a539683b7c25f2048b95dc9b
temashidzo <temashidzo@gmail.com>
2024-03-27 22:00:12 -0500

    add script for telegram bot

---
+++ scripts/telegram.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source /Users/astonuser/Documents/Docs/Projects/Termin/.venv/bin/activate
+nohup python3 -u /Users/astonuser/Documents/Docs/Projects/Termin/src/termin_bot.py >> /Users/astonuser/Documents/Docs/Projects/Termin/logs/telegram.log &
